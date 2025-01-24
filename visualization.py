c83ea46b4835f160b0540ecf92bc9b8057eb0ba5
Insung Kwon <insung.kwon@sciencespo.fr>
2025-01-24 13:55:49 +0100

    Skeleton for visualization

---
+++ visualization.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.header('What are customers buying from our app?')
+st.subheader('A Deepdive on Instacart Online Grocery Shopping Dataset')
+
+with st.sidebar:
+    st.title("")
+    st.title("")
+    st.title("")
+    st.subheader("Purchase combinations")
+    pc1 = st.button("Which product groups are purchased together most frequently?")
+
+    st.button("How do purchasing patterns for these product groups vary at different times of the day?")
+
+if pc1:
+    st.header('placeholder')
+# How do purchasing patterns for these product groups vary at different times of the day?
+# Purchase timing and frequency
+
+# What are the peak purchasing times during the week?
+# Which products are most commonly reordered by customers?
+# How frequently are products reordered on average?
+# Individual product analysis
+
+# Which products are typically bought as standalone items?
+# What are the products that customers rarely or never reorder?
+# Are there products that are seldom or never purchased?")
