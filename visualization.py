8f2e929caf7f4d6f108515ad068ae0444d8fbd57
Insung Kwon <insung.kwon@sciencespo.fr>
2025-01-27 19:51:32 +0100

    Filling in visualization tabs

--- visualization.py
+++ visualization.py
@@ -2,22 +2,8 @@ import streamlit as st
 import pandas as pd
 from pyvis import network as net
 import networkx as nx
-import streamlit.components.v1 as components
+
 from stvis import pv_static
-
-
-
-button_html = """
-    <style>
-        .btn {
-            text-align: center;  /* Center the text */
-            line-height: 50px;  /* Align text vertically */
-            display: block;
-            margin: 0 auto;  /* Center the button horizontally */
-        }
-    </style>
-    <a href='#' class='btn'>Click me!</a>
-"""
 
 
 st.header('What are customers buying from our app?')
@@ -29,74 +15,72 @@ with st.sidebar:
     st.title("")
     st.title("")
     st.title("")
+
     st.subheader("Purchase combinations")
-    pc1 = st.button("Which product groups are purchased together most frequently?")
+    q1 = st.button("Which product groups are purchased together most frequently?")
+    q2 = st.button("How do purchasing patterns for these product groups vary at different times of the day?")
 
-    pc2 = st.button("How do purchasing patterns for these product groups vary at different times of the day?")
-    pc100 = st.button('Test')
+    st.subheader("Purchase timing and frequency")
+    q3 = st.button("What are the peak purchasing times during the week?")
+    q4 = st.button("Which products are most commonly reordered by customers?")
+    q5 = st.button("How frequently are products reordered on average?")
+    q6 = st.button("What are the products that customers rarely or never reorder?")
 
-# How do purchasing patterns for these product groups vary at different times of the day?
-# Purchase timing and frequency
+    st.subheader("Purchase timing and frequency")
+    q3 = st.button("Which products are typically bought as standalone items?")
+    q4 = st.button("Are there products that are seldom or never purchased?")
 
-# What are the peak purchasing times during the week?
-# Which products are most commonly reordered by customers?
-# How frequently are products reordered on average?
-# Individual product analysis
+data1  = pd.read_csv('q1-1.csv')
+data2 = pd.read_csv('q1-2.csv')
+data3 = pd.read_csv('q2-1.csv')
+data4 = pd.read_csv('q2-2.csv')
+data5 = pd.read_csv('q2-3.csv')
 
-# Which products are typically bought as standalone items?
-# What are the products that customers rarely or never reorder?
-# Are there products that are seldom or never purchased?")
-import pandas as pd
+def network_graph(df):
+        G = nx.from_pandas_edgelist(df, source='antecedent', target='consequent',
+                                edge_attr='support_itemset_relative_pct')
 
-data_1  = pd.read_csv('q1-1.csv')
-data_2 = pd.read_csv('q1-2.csv')
+        prod_net = net.Network(notebook = True, cdn_resources='in_line',
+                            height="1000px",
+                            width="1000px",
+                            font_color="black")
 
-if pc1:
-    G = nx.from_pandas_edgelist(data_1, source='antecedent', target='consequent',
-                            edge_attr='support_itemset_relative_pct')
+        prod_net.barnes_hut()
 
-    got_net = net.Network(notebook = True, cdn_resources='in_line',
-                        height="1000px",
-                        width="1000px",
-                        font_color="black")
+        sources = df.antecedent
+        targets = df.consequent
+        weights = df.confidence_pct
 
-    got_net.barnes_hut()
+        edge_data = zip(sources, targets, weights)
 
-    sources = data_1.antecedent
-    targets = data_1.consequent
-    weights = data_1.confidence_pct
+        for e in edge_data:
+                        src = e[0]
+                        dst = e[1]
+                        w = e[2]
 
-    edge_data = zip(sources, targets, weights)
+                        prod_net.add_node(src, src, title=src)
+                        prod_net.add_node(dst, dst, title=dst)
+                        prod_net.add_edge(src, dst, value=w)
 
-    for e in edge_data:
-                    src = e[0]
-                    dst = e[1]
-                    w = e[2]
+        neighbor_map = prod_net.get_adj_list()
 
-                    got_net.add_node(src, src, title=src)
-                    got_net.add_node(dst, dst, title=dst)
-                    got_net.add_edge(src, dst, value=w)
+        nx.set_node_attributes(G,df.support_itemset_relative_pct*30,'size')
 
-    neighbor_map = got_net.get_adj_list()
+        # add neighbor data to node hover data
+        for node in prod_net.nodes:
+                        node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
+                        node["value"] = len(neighbor_map[node["id"]])
+                        node["font"] = {'size' : 100}
 
-    nx.set_node_attributes(G,data_1.support_itemset_relative_pct*10,'size')
+        pv_static(prod_net)
 
-    # add neighbor data to node hover data
-    for node in got_net.nodes:
-                    node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
-                    node["value"] = len(neighbor_map[node["id"]])
-                    node["font"] = {'size' : 100}
+if q1:
+    network_graph(data1)
 
+if q2:
+    if 'hour' not in st.session_state:
+        st.session_state['hour'] = 0
 
-    pv_static(got_net)
-
-    HtmlFile = open("\\network_graph\product_network.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read()
-    components.html(source_code, height = 500,width=800)
-
-if pc2:
-    st.write(data_2)
-
-
-if pc100:
-    pass
+    st.session_state.hour =  st.selectbox("Select hour: ",(range(0,24)))
+    df = data2.loc[data2.hour == st.session_state.hour]
+    st.dataframe(df)
